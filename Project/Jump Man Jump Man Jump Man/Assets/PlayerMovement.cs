afd5d7bd4af4b38e211690e57c102ca408613e35
memonahad <memonahad@rocketmail.com>
2018-10-14 15:10:14 -0500

    Game Board Added

---
+++ Project/Jump Man Jump Man Jump Man/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float moveX;
+    public float moveY;
+    public int playerSpeed = 10;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, moveY * playerSpeed);
+    }
+}
